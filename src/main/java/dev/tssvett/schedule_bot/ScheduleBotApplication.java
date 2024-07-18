a29826cb4b153004063989ded1a30ec6eb7e6508
tssvett <tssvett@mail.ru>
2024-07-18 18:52:43 +0300

    init

---
+++ src/main/java/dev/tssvett/schedule_bot/ScheduleBotApplication.java
@@ -0,0 +1,13 @@
+package dev.tssvett.schedule_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tssvett/schedule_bot/ScheduleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tssvett.schedule_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
