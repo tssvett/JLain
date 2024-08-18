3f8173e65cac93bae9e5b4403eebe2ba1ba79bd4
tssvett <tssvett@mail.ru>
2024-08-18 17:17:36 +0300

    added annotations to notification command

--- src/main/java/dev/tssvett/schedule_bot/actions/command/impl/NotificationCommand.java
+++ src/main/java/dev/tssvett/schedule_bot/actions/command/impl/NotificationCommand.java
@@ -2,6 +2,8 @@ package dev.tssvett.schedule_bot.actions.command.impl;
 
 import dev.tssvett.schedule_bot.actions.command.Command;
 import dev.tssvett.schedule_bot.actions.keyboard.impl.NotificationKeyboard;
+import dev.tssvett.schedule_bot.annotation.DirectMessageRequired;
+import dev.tssvett.schedule_bot.annotation.RegistrationRequired;
 import dev.tssvett.schedule_bot.enums.Action;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +16,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 public class NotificationCommand implements Command {
     private final NotificationKeyboard notificationKeyboard;
     @Override
+    @DirectMessageRequired
+    @RegistrationRequired
     public SendMessage execute(Long userId, Long chatId) {
         log.info("Received " + this.getClass().getSimpleName() + " from userId: {}", userId);
         return SendMessage.builder()
